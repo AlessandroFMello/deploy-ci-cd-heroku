05c393075f8e456743c6a0a6110fdd737b731ef6
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-03-17 10:18:34 -0300

    iniciando heroku com docker

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => res.send(`<h1>Executando na porta ${port}</h1>`));
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
